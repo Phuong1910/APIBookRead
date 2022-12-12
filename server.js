4867c57594d6fe8d819a2c69a7d1c07530e39041
Phuong1910 <91041286+Phuong1910@users.noreply.github.com>
2022-12-12 09:39:18 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require ('http')
+const { getProducts, getProductById, getProductByTitle, getProductByAuthor, getProductByNhanVat, createProduct, updateProduct, deleteProduct } = require('./controllers/productController')
+
+const server = http.createServer((req,res) => {
+    if (req.url === '/api/products' && req.method === 'GET'){
+        getProducts(req, res)
+   } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET' ){
+        const id = req.url.split('/')[3]
+        getProductById(req,res,id)
+   } else if (req.url.match(/\/api\/products\/title\/([a-zA-Z]+)/) && req.method === 'GET' ){
+          const title = decodeURI(req.url.split('/')[4]).toLowerCase()
+          getProductByTitle(req,res,title)
+   } else if (req.url.match(/\/api\/products\/author\/([a-zA-Z ]+)/) && req.method === 'GET' ){
+          const author = decodeURI(req.url.split('/')[4]).toLowerCase()
+          getProductByAuthor(req,res,author)
+   } else if (req.url.match(/\/api\/products\/character\/([a-zA-Z ]+)/) && req.method === 'GET' ){
+          const descriptionNhanVat = decodeURI(req.url.split('/')[4]).toLowerCase()
+          getProductByNhanVat(req,res,descriptionNhanVat)
+   } else if (req.url === '/api/products/add' && req.method === 'POST' ){
+          console.log(req.url)
+        createProduct(req,res)
+   } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT' ){
+        const id = req.url.split('/')[3]
+        updateProduct(req,res,id)
+   } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE' ){
+        const id = req.url.split('/')[3]
+        deleteProduct(req,res,id)
+   } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({message: 'Product Not Found'}))
+   }
+})
+
+const PORT =  process.env.PORT || 5000
+
+server.listen(PORT,() => console.log(`Server running on port ${PORT}`))
